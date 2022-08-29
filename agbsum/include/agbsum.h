f427dd0a43c976a8ffdd90469d5b84ef831a0845
Gabriel Jensen <gabrielbjensen@icloud.com>
2022-08-29 19:14:59 +0200

    Fix version number not being updated;

--- agbsum/include/agbsum.h
+++ agbsum/include/agbsum.h
@@ -21,7 +21,7 @@
 #define nullptr NULL
 #define typeof __typeof__
 
-constexpr uint_least64_t agbsum_ver = 0x2u;
+constexpr uint_least64_t agbsum_ver = 0x5u;
 
 constexpr size_t agbsum_romstart = 0xA0u;
 
